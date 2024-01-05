8226d6a351709e5b134277949fc08044d2ececd2
Lucas Quaresma <lucas.quaresma1@gmail.com>
2024-01-05 07:43:54 -0300

    word_couund discretizado por ano ok

--- word_count.sql
+++ word_count.sql
@@ -16,13 +16,14 @@ splited AS (
 
 exp_detalhe AS (
     SELECT 
-        word, 
+        word,
+        anogeracao,
         SUM(COALESCE(CAST(detalhe AS FLOAT), 0)) AS detalhe
-
     FROM 
     (
         SELECT 
-            explode(detalhe) AS word, 
+            explode(detalhe) AS word,
+            anogeracao,
             CASE 
                 WHEN quantidadegerada IS NULL OR quantidadegerada = '' THEN '0'
                 ELSE quantidadegerada
@@ -30,19 +31,20 @@ exp_detalhe AS (
         FROM splited
     ) AS exploded_words
     WHERE NOT (word RLIKE '^[0-9]+([,.][0-9]+)?$') AND word != ''
-    GROUP BY word
+    GROUP BY word, anogeracao
     ORDER BY word DESC
 ),
 
 exp_tiporesiduo AS (
     SELECT 
-        word, 
+        word,
+        anogeracao,
         SUM(COALESCE(CAST(tiporesiduo AS FLOAT), 0)) AS tiporesiduo
-
     FROM 
     (
         SELECT 
-            explode(tiporesiduo) AS word, 
+            explode(tiporesiduo) AS word,
+            anogeracao,
             CASE 
                 WHEN quantidadegerada IS NULL OR quantidadegerada = '' THEN '0'
                 ELSE quantidadegerada
@@ -50,19 +52,20 @@ exp_tiporesiduo AS (
         FROM splited
     ) AS exploded_words
     WHERE NOT (word RLIKE '^[0-9]+([,.][0-9]+)?$') AND word != ''
-    GROUP BY word
+    GROUP BY word, anogeracao
     ORDER BY word DESC
 ),
 
 exp_categoriaatividade AS (
     SELECT 
-        word, 
+        word,
+        anogeracao,
         SUM(COALESCE(CAST(categoriaatividade AS FLOAT), 0)) AS categoriaatividade
-
     FROM 
     (
         SELECT 
-            explode(categoriaatividade) AS word, 
+            explode(categoriaatividade) AS word,
+            anogeracao,
             CASE 
                 WHEN quantidadegerada IS NULL OR quantidadegerada = '' THEN '0'
                 ELSE quantidadegerada
@@ -70,19 +73,20 @@ exp_categoriaatividade AS (
         FROM splited
     ) AS exploded_words
     WHERE NOT (word RLIKE '^[0-9]+([,.][0-9]+)?$') AND word != ''
-    GROUP BY word
+    GROUP BY word, anogeracao
     ORDER BY word DESC
 ),
 
 exp_razaosocialgerador AS (
     SELECT 
-        word, 
+        word,
+        anogeracao,
         SUM(COALESCE(CAST(razaosocialgerador AS FLOAT), 0)) AS razaosocialgerador
-
     FROM 
     (
         SELECT 
-            explode(razaosocialgerador) AS word, 
+            explode(razaosocialgerador) AS word,
+            anogeracao,
             CASE 
                 WHEN quantidadegerada IS NULL OR quantidadegerada = '' THEN '0'
                 ELSE quantidadegerada
@@ -90,7 +94,7 @@ exp_razaosocialgerador AS (
         FROM splited
     ) AS exploded_words
     WHERE NOT (word RLIKE '^[0-9]+([,.][0-9]+)?$') AND word != ''
-    GROUP BY word
+    GROUP BY word, anogeracao
     ORDER BY word DESC
 )
 
@@ -101,6 +105,12 @@ SELECT
         exp_categoriaatividade.word, 
         exp_razaosocialgerador.word
         ) AS word,
+    COALESCE(
+        exp_detalhe.anogeracao, 
+        exp_tiporesiduo.anogeracao, 
+        exp_categoriaatividade.anogeracao, 
+        exp_razaosocialgerador.anogeracao
+        ) AS anogeracao,  
     COALESCE(detalhe, 0) AS detalhe,
     COALESCE(tiporesiduo, 0) AS tiporesiduo,
     COALESCE(categoriaatividade, 0) AS categoriaatividade,
@@ -108,12 +118,119 @@ SELECT
 
 FROM exp_detalhe 
 FULL OUTER JOIN exp_tiporesiduo 
-ON exp_detalhe.word = exp_tiporesiduo.word
+ON exp_detalhe.word = exp_tiporesiduo.word 
+AND exp_detalhe.anogeracao = exp_tiporesiduo.anogeracao
 
 FULL OUTER JOIN exp_categoriaatividade 
 ON COALESCE(exp_detalhe.word, exp_tiporesiduo.word) = exp_categoriaatividade.word
+AND COALESCE(exp_detalhe.anogeracao, exp_tiporesiduo.anogeracao) = exp_categoriaatividade.anogeracao
 
 FULL OUTER JOIN exp_razaosocialgerador 
 ON COALESCE(exp_detalhe.word, exp_tiporesiduo.word, exp_categoriaatividade.word) = exp_razaosocialgerador.word
+AND COALESCE(exp_detalhe.anogeracao, exp_tiporesiduo.anogeracao, exp_categoriaatividade.anogeracao) = exp_razaosocialgerador.anogeracao
 
 ;
+
+
+
+-- exp_detalhe AS (
+--     SELECT 
+--         word, 
+--         SUM(COALESCE(CAST(detalhe AS FLOAT), 0)) AS detalhe
+
+--     FROM 
+--     (
+--         SELECT 
+--             explode(detalhe) AS word, 
+--             CASE 
+--                 WHEN quantidadegerada IS NULL OR quantidadegerada = '' THEN '0'
+--                 ELSE quantidadegerada
+--             END AS detalhe
+--         FROM splited
+--     ) AS exploded_words
+--     WHERE NOT (word RLIKE '^[0-9]+([,.][0-9]+)?$') AND word != ''
+--     GROUP BY word
+--     ORDER BY word DESC
+-- ),
+
+-- exp_tiporesiduo AS (
+--     SELECT 
+--         word, 
+--         SUM(COALESCE(CAST(tiporesiduo AS FLOAT), 0)) AS tiporesiduo
+
+--     FROM 
+--     (
+--         SELECT 
+--             explode(tiporesiduo) AS word, 
+--             CASE 
+--                 WHEN quantidadegerada IS NULL OR quantidadegerada = '' THEN '0'
+--                 ELSE quantidadegerada
+--             END AS tiporesiduo
+--         FROM splited
+--     ) AS exploded_words
+--     WHERE NOT (word RLIKE '^[0-9]+([,.][0-9]+)?$') AND word != ''
+--     GROUP BY word
+--     ORDER BY word DESC
+-- ),
+
+-- exp_categoriaatividade AS (
+--     SELECT 
+--         word, 
+--         SUM(COALESCE(CAST(categoriaatividade AS FLOAT), 0)) AS categoriaatividade
+
+--     FROM 
+--     (
+--         SELECT 
+--             explode(categoriaatividade) AS word, 
+--             CASE 
+--                 WHEN quantidadegerada IS NULL OR quantidadegerada = '' THEN '0'
+--                 ELSE quantidadegerada
+--             END AS categoriaatividade
+--         FROM splited
+--     ) AS exploded_words
+--     WHERE NOT (word RLIKE '^[0-9]+([,.][0-9]+)?$') AND word != ''
+--     GROUP BY word
+--     ORDER BY word DESC
+-- ),
+
+-- exp_razaosocialgerador AS (
+--     SELECT 
+--         word, 
+--         SUM(COALESCE(CAST(razaosocialgerador AS FLOAT), 0)) AS razaosocialgerador
+
+--     FROM 
+--     (
+--         SELECT 
+--             explode(razaosocialgerador) AS word, 
+--             CASE 
+--                 WHEN quantidadegerada IS NULL OR quantidadegerada = '' THEN '0'
+--                 ELSE quantidadegerada
+--             END AS razaosocialgerador
+--         FROM splited
+--     ) AS exploded_words
+--     WHERE NOT (word RLIKE '^[0-9]+([,.][0-9]+)?$') AND word != ''
+--     GROUP BY word
+--     ORDER BY word DESC
+-- )
+
+-- SELECT 
+--     COALESCE(
+--         exp_detalhe.word, 
+--         exp_tiporesiduo.word, 
+--         exp_categoriaatividade.word, 
+--         exp_razaosocialgerador.word
+--         ) AS word,
+--     COALESCE(detalhe, 0) AS detalhe,
+--     COALESCE(tiporesiduo, 0) AS tiporesiduo,
+--     COALESCE(categoriaatividade, 0) AS categoriaatividade,
+--     COALESCE(razaosocialgerador, 0) AS razaosocialgerador
+
+-- FROM exp_detalhe 
+-- FULL OUTER JOIN exp_tiporesiduo 
+-- ON exp_detalhe.word = exp_tiporesiduo.word
+
+-- FULL OUTER JOIN exp_categoriaatividade 
+-- ON COALESCE(exp_detalhe.word, exp_tiporesiduo.word) = exp_categoriaatividade.word
+
+-- FULL OUTER JOIN exp_razaosocialgerador 
+-- ON COALESCE(exp_detalhe.word, exp_tiporesiduo.word, exp_categoriaatividade.word) = exp_razaosocialgerador.word
